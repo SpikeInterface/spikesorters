169ad635be300b8f523ef0cf1a1e56dde2e4d36c
colehurwitz <colehurwitz@gmail.com>
2019-09-15 16:50:29 +0100

    adjust type

--- spikesorters/basesorter.py
+++ spikesorters/basesorter.py
@@ -33,7 +33,7 @@ class BaseSorter:
     SortingExtractor_Class = None  # convinience to get the extractor
     _default_params = {}
     sorter_gui_params = [
-        {'name': 'output_folder', 'type': 'str', 'value':None, 'default':None,  'title': "Sorting output folder path", 'base_param':True},
+        {'name': 'output_folder', 'type': 'folder', 'value':None, 'default':None,  'title': "Sorting output folder path", 'base_param':True},
         {'name': 'grouping_property', 'type': 'str', 'value':None, 'default':None,  'title': "Will sort the recording by the given property ('group', etc.)", 'base_param':True},
         {'name': 'parallel', 'type': 'bool', 'value':False, 'default':False,  'title': "If the recording is sorted by a property, then it will do this in parallel", 'base_param':True},
         {'name': 'delete_output_folder', 'type': 'bool', 'value':False, 'default':False, 'title': "If True, delete the results of the sorter, otherwise, it won't.", 'base_param':True},
