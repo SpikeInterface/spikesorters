ce89b37d2aa2bfb9529ac52779e3165b43298206
Samuel Garcia <sam.garcia.die@gmail.com>
2020-01-28 14:41:45 +0100

    add HS2 version

---
+++ spikesorters/tests/test_sorterlist.py
@@ -0,0 +1,24 @@
+import os
+kilosort2_path = '/home/samuel/Documents/Spikeinterface/Kilosort2'
+os.environ["KILOSORT2_PATH"] = kilosort2_path
+
+kilosort_path = '/home/samuel/Documents/Spikeinterface/KiloSort/'
+os.environ["KILOSORT_PATH"] = kilosort_path
+
+ironclust_path = '/home/samuel/Documents/Spikeinterface/ironclust'
+os.environ["IRONCLUST_PATH"] = ironclust_path
+
+
+import pytest
+
+from spikesorters import print_sorter_version
+
+
+def test_print_sorter_version():
+    print_sorter_version()
+
+
+
+
+if __name__ == '__main__':
+    test_print_sorter_version()
