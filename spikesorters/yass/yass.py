ceb4656771a96ab0e7078672734ae58d973b1297
cat <cat>
2020-12-09 19:01:49 +0100

    removed static method

--- spikesorters/yass/yass.py
+++ spikesorters/yass/yass.py
@@ -25,19 +25,35 @@ class YassSorter(BaseSorter):
     sorter_name = 'yass'
     requires_locations = False
 
-    _default_params = {
-        }
+    _default_params = {'neural_nets_path':None}
 
-    _params_description = {
-    }
+    _params_description = { }
        
-    sorter_description = """Yass description; link to biorxiv"""
+    sorter_description = """Yass is a deconvolution and neural network based spike sorting algorithm designed for
+                            recordings with no drift (such as retinal recordings).
+                            
+                            For more information see https://www.biorxiv.org/content/10.1101/2020.03.18.997924v1"""
 
-    installation_mesg = """\nTo use Yass run:\n
-        >>> pip install yass-algorithm
+    installation_mesg = """\nTo install Yass run:\n
+                            pip install yass-algorithm
+                            
+                            Yass can be run in 2 modes:
+                            
+                            1.  Retraining Neural Networks (Default)
+                            
+                            import spikesorters as ss
+                            import spikeextractors as se
+                            rec, sort = se.example_datasets.toy_example(duration=300)
+                            sorting_yass = ss.run_yass(rec, '/home/cat/Downloads/test2')
 
-        More information ...
-    """
+
+                            2.  Using previously trained Neural Networks:
+                            ...
+                            sorting_yass = ss.run_yass(rec, '/home/cat/Downloads/test2', neural_nets_path=PATH_TO_NEURAL_NETS)
+                            
+                            For any installation or operation issues please visit: https://github.com/paninski-lab/yass
+
+                        """
 
     def __init__(self, **kargs):
         BaseSorter.__init__(self, **kargs)
@@ -47,6 +63,8 @@ class YassSorter(BaseSorter):
         with open(config_default_location) as file:
             self.yass_params = yaml.load(file, Loader=yaml.FullLoader)
     
+        self.neural_nets_path = None
+        
     @classmethod
     def is_installed(cls):
         return HAVE_YASS
@@ -57,8 +75,7 @@ class YassSorter(BaseSorter):
 
     def _setup_recording(self, recording, output_folder):
         p = self.params
-        source_dir = Path(output_folder).parent
-        
+                
         #################################################################
         #################### UPDATE ROOT FOLDER #########################
         #################################################################
@@ -104,13 +121,27 @@ class YassSorter(BaseSorter):
         #################################################################
         ############ RUN NN TRAINING ON EXISTING DATASET ################
         #################################################################
-        self.train(recording, output_folder)
-        
-        #################################################################
-        ############ RUN NN TRAINING ON EXISTING DATASET ################
-        #################################################################
-        self.NNs_update_location(output_folder)
+        self.neural_nets_path = p['neural_nets_path']
 
+        if self.neural_nets_path is None:
+            # retrain NNs
+            self.train(recording, output_folder)
+            
+            # update NN folder location
+            neural_nets_path = os.path.join(output_folder, 
+                                            'tmp',
+                                            'nn_train')
+
+        #################################################################
+        ####################### OR LOAD PREVIOUS NNS ####################
+        #################################################################
+        else:
+            print ("USING PREVIOUSLY TRAINED NNs FROM THIS LOCATION: ",
+                    self.neural_nets_path)
+            # use previuosly trained NN folder location
+            neural_nets_path = self.neural_nets_path
+        
+        self.NNs_update_location(output_folder, neural_nets_path)
 
         #self.fname_config = fname_config
         
@@ -184,7 +215,7 @@ class YassSorter(BaseSorter):
                 output_folder,"/tmp/nn_train/denoise.pt")        
         
         
-    def NNs_update_location(self, output_folder):
+    def NNs_update_location(self, output_folder, neural_nets_path):
         ''' Update NNs to newly trained ones prior to running yass sort
         '''
         
@@ -192,16 +223,15 @@ class YassSorter(BaseSorter):
         #################### UPDATE CONFIG FILE TO NEW NNS ##############
         #################################################################
         self.yass_params['neuralnetwork']['denoise']['filename']= os.path.join(
-                                            output_folder, 
-                                            'tmp',
-                                            'nn_train',
-                                            'denoise.pt')
+                                                                    neural_nets_path,
+                                                                    'denoise.pt')
+                                                                    
         self.yass_params['neuralnetwork']['detect']['filename']= os.path.join(
-                                            output_folder, 
-                                            'tmp',
-                                            'nn_train',
-                                            'detect.pt')
-        
+                                                                    neural_nets_path,
+                                                                    'detect.pt')
+                                                                    
+                                                                    
+                                                                    
         #################################################################
         #################### SAVE UPDATED CONFIG FILE ###################
         #################################################################
@@ -230,7 +260,3 @@ class YassSorter(BaseSorter):
         with open(fname_config, 'w') as file:
             documents = yaml.dump(self.yass_params, file)   
             
-    @staticmethod
-    def get_result_from_folder(output_folder):
-        sorting = se.SpykingCircusSortingExtractor(folder_path=Path(output_folder) / 'recording')
-        return sorting
