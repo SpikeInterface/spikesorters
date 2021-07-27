2a97545b2a158ffabf95db0791be3faf3fe1b5d0
Alessio Buccino <alejoe9187@gmail.com>
2021-07-27 20:23:15 +0200

    tdc installation issues

--- spikesorters/tests/test_tridesclous.py
+++ spikesorters/tests/test_tridesclous.py
@@ -5,12 +5,6 @@ import spikeextractors as se
 from spikesorters import TridesclousSorter, run_tridesclous
 from spikesorters.tests.common_tests import SorterCommonTestSuite
 
-try:
-    import matplotlib
-    matplotlib.use("agg")
-except:
-    pass
-
 
 # This run several tests
 @pytest.mark.skipif(not TridesclousSorter.is_installed(), reason='tridesclous not installed')
